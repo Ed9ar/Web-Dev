c3944e9a8b457d499df2fa199bd8c58c500adfb3
ed9ar <a01339939@itesm.mx>
2020-04-05 19:03:58 -0500

    Finished
    Finished

--- Django/portfolio/jobs/models.py
+++ Django/portfolio/jobs/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=200)
+    #url = models.TextField(default='hoihioh')
 
 
+    
